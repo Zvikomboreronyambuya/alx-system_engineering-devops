da36292dd0f1a97d5e53f5f82233cba35c073631
Zvikomborero Nyambuya <znyambuya@gmail.com>
2024-03-05 09:49:16 +0000

    changed query info

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
-import requests
+"""Module that queries the reddit api"""
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """script that queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
+
+    query_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-Agent"},
+                            allow_redirects=False)
+    if query_info.status_code >= 300:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    return query_info.json().get("data").get("subscribers")
